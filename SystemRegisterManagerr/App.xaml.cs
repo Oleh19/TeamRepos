d90b529870dcad641d962129c212fb375a15140b
Student <student@lan.itstep.kiev.ua>
2018-10-30 19:45:51 +0200

    first commit

---
+++ SystemRegisterManagerr/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SystemRegisterManagerr
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
